a3e0365b0b43b3c2dc7b680fcd449da0057b955b
gupta-parag <parag.gupta@protonmail.com>
2023-10-25 22:26:39 -0400

    Added chapter 2 readme file

--- bootcamp/Ch2_JSX/JSXconds/app.js
+++ bootcamp/Ch2_JSX/JSXconds/app.js
@@ -9,6 +9,11 @@ class ComponentOne extends React.Component {
       <div>
         <h1>The random number is : {num}</h1>
         <p>{num === 5 ? "You are Lucky !!!" : "You are doomed"}</p>
+        <figure>
+          {num === 5 && (
+            <img src="https://images.unsplash.com/photo-1573995975633-faee0123f31f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          )}
+        </figure>
       </div>
     );
   }
